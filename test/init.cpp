6b5882aadd1e7e791498afeaee457adac9d7d229
NickolasDePiton <crysis4997@ya.ru>
2016-12-04 20:06:53 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -5,71 +5,31 @@
 #include <fstream>
 using namespace std;
 
-SCENARIO("8mb", "[8mb]")
-{
-  Vnesh_sort("8mb", "out_8", 1);
-  bool x = true;
-  ifstream f1("out_8"), f2("out8");
-  line s1, s2;
-  while(!f2.eof() && !f1.eof())
-  {
-    if((f1>>s1)&&(f2>>s2))
-    {
-      if(s1.name!=s2.name)
-      {
-        x=false;
-        break;
-      }
-    }
-    else break;
-  }
-  f1.close();
-  f2.close();
-  REQUIRE(x);
-}
-
-SCENARIO("15mb", "[15mb]")
-{
-  Vnesh_sort("15mb", "out_15", 4);
-  bool x = true;
-  ifstream f1("out_15"), f2("out15");
-  line s1, s2;
-  while(!f2.eof() && !f1.eof())
-  {
-    if((f1>>s1)&&(f2>>s2))
-    {
-      if(s1.name!=s2.name)
-      {
-        x=false;
-        break;
-      }
-    }
-    else break;
-  }
-  f1.close();
-  f2.close();
-  REQUIRE(x);
-}
-
 SCENARIO("32mb", "[32mb]")
 {
-  Vnesh_sort("32mb", "out_32", 17);
-  bool x = true;
-  ifstream f1("out_32"), f2("out32");
-  line s1, s2;
-  while(!f2.eof() && !f1.eof())
-  {
-    if((f1>>s1)&&(f2>>s2))
-    {
-      if(s1.name!=s2.name)
-      {
-        x=false;
-        break;
-      }
-    }
-    else break;
-  }
-  f1.close();
-  f2.close();
-  REQUIRE(x);
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+	Vnesh_sort("32mb", "out_32", 17);
+	end = std::chrono::system_clock::now();
+	cout <<"32MB - " <<(end - start).count() <<" ns"<< endl;
+  REQUIRE(1);
+}
+SCENARIO("15mb", "[15mb]")
+{
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+	Vnesh_sort("15mb", "out_15", 4);
+	end = std::chrono::system_clock::now();
+	cout <<"15MB - " <<(end - start).count() <<" ns"<< endl;
+  REQUIRE(1);
+}
+
+SCENARIO("8mb", "[8mb]")
+{
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+	Vnesh_sort("8mb", "out_8", 1);
+	end = std::chrono::system_clock::now();
+	cout <<"8MB - " <<(end - start).count() <<" ns"<< endl;
+  REQUIRE(1);
 }
