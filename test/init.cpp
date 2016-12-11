07d8ab16c446974628f5d9541d0b41adc73ad0d7
NickolasDePiton <crysis4997@ya.ru>
2016-12-11 16:31:03 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -13,26 +13,27 @@ SCENARIO("32mb", "[32mb]")
 	Vnesh_sort("32mb", "out_32", 17);
 	end = std::chrono::system_clock::now(); 
 	int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-	std::cout << elapsed_seconds << std::endl;
-	cout <<"32MB - " <<(end - start).count() <<" ms"<< endl;
+	std::cout <<"32MB - " << elapsed_seconds <<" ms" << std::endl;
   REQUIRE(1);
 }
 SCENARIO("15mb", "[15mb]")
 {
 	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+        start = std::chrono::system_clock::now();
 	Vnesh_sort("15mb", "out_15", 4);
-	end = std::chrono::system_clock::now();
-	cout <<"15MB - " <<(end - start).count() <<" ns"<< endl;
+	end = std::chrono::system_clock::now(); 
+	int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+	std::cout <<"15MB - " << elapsed_seconds <<" ms" << std::endl;
   REQUIRE(1);
 }
 
 SCENARIO("8mb", "[8mb]")
 {
 	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+        start = std::chrono::system_clock::now();
 	Vnesh_sort("8mb", "out_8", 1);
-	end = std::chrono::system_clock::now();
-	cout <<"8MB - " <<(end - start).count() <<" ns"<< endl;
+	end = std::chrono::system_clock::now(); 
+	int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+	std::cout <<"8MB - " << elapsed_seconds <<" ms" << std::endl;
   REQUIRE(1);
 }
